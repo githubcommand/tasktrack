01e5e01ec248f822523936686a484449ddfb63c0
githubcommand <githubcommand@gmail.com>
2024-04-12 14:54:59 +0530

    ini

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
+import React from "react";
 import "./Tag.css";
 
-const Tag = ({tagName,selectTag,selected}) => {
-    const tagStyle ={
-        HTML:{
-            backgroundColor:'#fda821'
-        },
-        CSS3:{
-            backgroundColor:'#15d4c8'
-        },
-        Javascript:{
-            backgroundColor:'#ffd12c'
-        },
-        React:{
-            backgroundColor:'#4cdafc'
-        },
-        default:{
-            backgroundColor:"#f9f9f9"
-        }
-
-    }
+const Tag = ({ tagName, selectTag, selected }) => {
+  const tagStyle = {
+    HTML: {
+      backgroundColor: "#fda821",
+    },
+    CSS3: {
+      backgroundColor: "#15d4c8",
+    },
+    Javascript: {
+      backgroundColor: "#ffd12c",
+    },
+    React: {
+      backgroundColor: "#4cdafc",
+    },
+    default: {
+      backgroundColor: "#f9f9f9",
+    },
+  };
   return (
-    <button style={selected?tagStyle[tagName]:tagStyle.default} type="button" onClick={()=>selectTag(tagName)} className="tag">{tagName}</button>
-    )
-}
+    <button
+      style={selected ? tagStyle[tagName] : tagStyle.default}
+      type="button"
+      onClick={() => selectTag(tagName)}
+      className="tag"
+    >
+      {tagName}
+    </button>
+  );
+};
 
-export default Tag
+export default Tag;
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,28 +1,23 @@
-import React from 'react'
+import React from "react";
 import "./TaskCard.css";
 import Tag from "./Tag";
-import deleteIcon from '../assets/delete.png';
-const TaskCard = ({title,tags,handleDelete,index}) => {
+import deleteIcon from "../assets/delete.png";
+const TaskCard = ({ title, tags, handleDelete, index }) => {
   return (
-    <article className='task_card'>
-        <p className='task_text'>
-            {title}
-        </p>
-        <div className='task_card_bottom_line'>
-            <div className="task_card_tags">
-             {
-                tags.map((tag,index)=>{
-                    return <Tag key={index} tagName={tag} selected/>
-                })
-             }
-
-            </div>
-            <div className="task_delete" onClick={() => handleDelete(index)}>
-                <img className="delete_icon" src={deleteIcon}/>
-            </div>
+    <article className="task_card">
+      <p className="task_text">{title}</p>
+      <div className="task_card_bottom_line">
+        <div className="task_card_tags">
+          {tags.map((tag, index) => {
+            return <Tag key={index} tagName={tag} selected />;
+          })}
         </div>
+        <div className="task_delete" onClick={() => handleDelete(index)}>
+          <img className="delete_icon" src={deleteIcon} />
+        </div>
+      </div>
     </article>
-  )
-}
+  );
+};
 
-export default TaskCard
+export default TaskCard;
