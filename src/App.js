ebcc19082afc35e9d4aafac6e0fb28e13b713b7a
githubcommand <githubcommand@gmail.com>
2024-04-12 14:51:07 +0530

    ini

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "./App.css";
 import TaskForm from "./components/TaskForm";
 import TaskColumn from "./components/TaskColumn";
@@ -9,7 +9,10 @@ import doneIcon from "./assets/check-mark-button.png";
 const oldTasks = localStorage.getItem('tasks');
 
 const App = () => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(JSON.parse(oldTasks) || []);
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+  },[tasks])
   const handleDelete = (taskIndex) => {
     const newTasks = tasks.filter((task, index) => {
       return index !== taskIndex;
